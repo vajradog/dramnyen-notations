eedbdedd3bb045839b56e34b4c3a93a674d8d2e9
Thupten N Chakrishar <vajradog@users.noreply.github.com>
2025-04-29 08:28:24 -0400

    updated to artifact v4

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-static'; // Changed from adapter-auto to adapter-static
+import adapter from '@sveltejs/adapter-static'; // Changed from adapter-auto
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
